0cee1229a4d3eb5bec8c5d93635c1ea6d6b2b391
james2doyle <james2doyle@gmail.com>
2016-11-18 14:00:17 -0800

    add initial files

---
+++ checkbox.js
@@ -0,0 +1,40 @@
+window.Vue.component('checkbox', {
+  props: {
+    name: {
+      type: String,
+      required: false
+    },
+    className: {
+      type: String,
+      required: false
+    },
+    id: {
+      type: String,
+      required: false
+    },
+    value: {
+      type: String,
+      required: false
+    },
+    required: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    checked: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    inverted: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+  },
+  template: "<div class=\"custom-checkbox\" v-bind:class=\"{ inverted: inverted }\"><input type=\"checkbox\" v-bind:name=\"name\" v-bind:class=\"className\" v-bind:id=\"id\" v-bind:value=\"value\" v-bind:checked=\"checked\" v-bind:required=\"required\"><label v-bind:for=\"id\">{{ label }}</label></div>"
+});
---
+++ index.js
@@ -0,0 +1,2 @@
+require('./checkbox.js');
+require('./radio.js');
